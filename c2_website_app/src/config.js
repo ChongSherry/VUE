7af345cfb24819cc8e8a4dd5c86e2a7138905527
ChongSherry <31024193+ChongSherry@users.noreply.github.com>
2017-09-08 14:29:14 +0800

    Add files via upload

---
+++ c2_website_app/src/config.js
@@ -0,0 +1,8 @@
+export default {
+    server: "http://192.168.1.163:3001",
+    port: {
+        find: `/service/find`,
+        findOne: `/service/findOn`,
+        content_hit: `/service/content_hit`
+    }
+}
---
+++ c2_website_app/src/pages/user.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: '这是用户中心'
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
